8774449f0d76ad4b0c49143b77654627f791f10e
李晨 <1366031854@qq.com>
2018-02-27 17:39:06 +0800

    ui小屏调试

--- qspos/src/constants/cashier/btnbefore.js
+++ qspos/src/constants/cashier/btnbefore.js
@@ -2,7 +2,7 @@ class Btnbrfore extends React.Component {
 	state={}
 	render(){
 		return(
-			<div style={{height:"90px",lineHeight:"90px",width:"90px",background:"#fff",fontSize:'26px',color:'#384162'}}>
+			<div className='reacge-before-one'>
 				{this.props.title}
     		</div>
 		)
