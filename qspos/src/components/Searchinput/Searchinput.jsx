d085095e6ccecc5773e00b4266b3991e10e4c00b
李晨 <1366031854@qq.com>
2018-06-06 17:12:33 +0800

    会员

--- qspos/src/components/Searchinput/Searchinput.jsx
+++ qspos/src/components/Searchinput/Searchinput.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Input} from 'antd';
+import {Input,Icon} from 'antd';
 
 class Searchinput extends React.Component {
 	state={
@@ -19,8 +19,8 @@ class Searchinput extends React.Component {
     render(){
         return(
            <div className='clearfix'>
-        		<Input  autoComplete="off" placeholder={this.props.text} className='fl f14 searchinput' style={{marginRight:'5px'}} onChange={this.hindchange.bind(this)} value={this.state.values} />
-        		<div className='fl tc f14 fff point searchinputbtn' onClick={this.hindsearch.bind(this)}>搜索</div>
+        		<Input  autoComplete="off" placeholder={this.props.text} className='fl f14 searchinput'  onChange={this.hindchange.bind(this)} value={this.state.values} />
+        		<div className='fl tc f14 fff point searchinputbtn' onClick={this.hindsearch.bind(this)}><Icon type="search"/><span style={{marginLeft:'5px'}}>搜索</span></div>
     		</div>
         )
     }
