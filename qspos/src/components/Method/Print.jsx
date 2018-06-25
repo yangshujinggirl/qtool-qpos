d143da0d4cbf78b484acc1995dfd921d622b611d
李晨 <1366031854@qq.com>
2018-06-25 15:36:13 +0800

    打印

--- qspos/src/components/Method/Print.jsx
+++ qspos/src/components/Method/Print.jsx
@@ -1251,7 +1251,7 @@ function printSaleOrder(message,printCount){
 
 	//添加支付方式
 	const payType="「微信扫码：1200.00   会员卡：31.20」"
-	LODOP.ADD_PRINT_TEXT(posi+30,"0mm","35mm",20,payType);
+	LODOP.ADD_PRINT_TEXT(posi+30,"0mm","70mm",20,payType);
 	LODOP.SET_PRINT_STYLEA(0,"FontName","微软雅黑");
 	LODOP.SET_PRINT_STYLEA(0,"FontSize",8);
 	LODOP.SET_PRINT_STYLEA(0,"Alignment",3);
@@ -1439,7 +1439,7 @@ function printSaleOrderSmall(message,printCount){
 
 	//添加支付方式
 	const payType="「微信扫码：1200.00   会员卡：31.20」"
-	LODOP.ADD_PRINT_TEXT(posi+30,"0mm","25mm",20,payType);
+	LODOP.ADD_PRINT_TEXT(posi+30,"0mm","50mm",20,payType);
 	LODOP.SET_PRINT_STYLEA(0,"FontName","微软雅黑");
 	LODOP.SET_PRINT_STYLEA(0,"FontSize",7);
 	LODOP.SET_PRINT_STYLEA(0,"Alignment",3);
