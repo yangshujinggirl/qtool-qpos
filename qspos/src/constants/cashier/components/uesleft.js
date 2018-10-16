eac7f19af09db993b7dfa9201387a309568390ab
yangshujing <1009838410@qq.com>
2018-10-16 13:57:18 +0800

    收银

--- qspos/src/constants/cashier/components/uesleft.js
+++ qspos/src/constants/cashier/components/uesleft.js
@@ -1,21 +1,58 @@
-import { Table, Input, Icon, Button, Popconfirm ,Tabs,Tooltip ,DatePicker,Select,message,Switch,Modal} from 'antd';
+import {
+	Table,
+	Input,
+	Icon,
+	Button,
+	Popconfirm ,
+	Tabs,
+	Tooltip ,
+	DatePicker,
+	Select,
+	message,
+	Switch,
+	Modal,
+	Radio,
+	Checkbox
+} from 'antd';
 import Modales from './model'
 import ReactDOM from 'react-dom';
 import { connect } from 'dva';
 import {GetServerData} from '../../../services/services';
+import {printRechargeOrder} from '../../../components/Method/Method';
+import Btnpay from './btnpay'
+import Btnbrfore from './btnbefore'
+import './uesleft.less';
 
+
+const columns = [{
+			title:'会员名称',
+			dataIndex: 'name',
+			key: 'name',
+	},{
+			title:'可用积分',
+			dataIndex: 'level',
+			key: 'level',
+	}]
 class Operationls extends React.Component {
-	state={
-		barcode:'',
-		onBlur:true,
-		cardNoMobile:'',
-		name:'',
-		levelStr:'',
-		integertotalamount:null,
-		checked:false,
-		cardNo:'',
-		mbCardId:null,
-    isBirthMonth:false
+	constructor(props) {
+		super(props);
+		this.state={
+			barcode:'',
+			onBlur:true,
+			cardNoMobile:'',
+			name:'',
+			levelStr:'',
+			integertotalamount:null,
+			checked:false,
+			cardNo:'',
+			mbCardId:null,
+	    isBirthMonth:false
+		}
+		this.rowSelection={
+			onChange:this.checkChange,
+			type:'radio'
+		}
+		this.firstclick=true
 	}
 	componentDidMount(){
 		this.focustap()
@@ -26,6 +63,12 @@ class Operationls extends React.Component {
 			}
 		})
 	}
+	componentDidUpdate(){
+   if(this.input){
+       const ValueorderNoses = ReactDOM.findDOMNode(this.input.input)
+       ValueorderNoses.focus()
+   }
+  }
 	//阻止默认事件
 	onKeydown=(e)=>{
 		if(e.keyCode==9){
@@ -129,12 +172,240 @@ class Operationls extends React.Component {
 			payload:{code:'qerp.pos.mb.card.find',values:values}
 		})
 	}
+	//切换会员
 	toggleEvent() {
-		
+		this.setState({ visible:true })
+		GetServerData('qerp.pos.mb.card.switch',{mobile:this.props.cardNoMobile})
+		.then((res) => {
+			if(res.code == '200') {
+				this.setState({ dataSource:res.iQposMbCards, visible:true });
+			}
+		},(error) => {
+			console.log(error)
+		})
+	}
+	onCancel() {
+		this.setState({ visible:false })
+	}
+	//选择会员
+	checkChange(selectedRowKeys, selectedRows) {
+		let cardNoMobile = selectedRows[0].key;
+		GetServerData('qerp.pos.mb.card.find',{ cardNoMobile })
+		.then((res) => {
+			if(res.code == '200') {
+				this.setState({ visible: false })
+			}
+		},(error) => {
+			console.log(error)
+		})
+	}
+	//充值
+	showModal = () => {
+		//判断有没有填写会员信息
+		if(this.props.mbCardId==null || undefined || ''){
+				message.warning('请输入正确的会员卡号')
+		}else{
+				const uservalues={"urUserId":null}
+				GetServerData('qerp.pos.ur.user.info',uservalues)
+				.then((json) => {
+						if(json.code=='0'){
+								sessionStorage.setItem('openWechat',json.urUser.shop.openWechat);
+								sessionStorage.setItem('openAlipay',json.urUser.shop.openAlipay);
+								GetServerData('qerp.pos.sy.config.info')
+								.then((json) => {
+									if(json.code == "0"){
+											if(json.config.rechargePrint=='1'){
+													const recheckPrint=true
+													this.props.dispatch({
+															type:'cashier/rechangeCheckPrint',
+															payload:recheckPrint
+													})
+											}else{
+													const recheckPrint=false
+													this.props.dispatch({
+															type:'cashier/rechangeCheckPrint',
+															payload:recheckPrint
+													})
+											}
+									}
+								})
+								const rechargevisible=true
+								this.props.dispatch({
+										type:'cashier/rechargevisible',
+										payload:rechargevisible
+								})
+						}
+				})
+		}
+	}
+	handleOk = (e) => {
+		if(this.firstclick){
+				this.firstclick=false
+		}else{
+				return
+		}
+		let values={mbCardId:this.props.mbCardId,amount:this.props.reamount,type:this.props.rechargetype}
+		GetServerData('qerp.pos.mb.card.charge',values)
+		.then((json) => {
+				if(json.code=='0'){
+						const rechargevisible=false
+						this.props.dispatch({
+								type:'cashier/rechargevisible',
+								payload:rechargevisible
+						})
+						const reamount=null
+						this.props.dispatch({
+								type:'cashier/reamount',
+								payload:reamount
+						})
+						setTimeout(()=>{
+								this.firstclick=true
+								this.searchmemberinfo()
+								message.success('充值成功',1)
+								const mbCardMoneyChargeIds=json.mbCardMoneyChargeId;
+								const chargeNos=json.chargeNo;
+								printRechargeOrder(this.props.recheckPrint,mbCardMoneyChargeIds)
+
+						},1)
+				}else{
+						message.warning(json.message)
+						this.firstclick=true
+				}
+		})
+	}
+		//打印
+	handprint = (id,type,orderNo,size) => {
+			GetLodop(id,type,orderNo,size)
+	}
+	handleCancel = (e) => {
+			const rechargevisible=false
+			this.props.dispatch({
+					type:'cashier/rechargevisible',
+					payload:rechargevisible
+			})
+	}
+	typelist=(index)=>{
+			if(index==1){
+					const typeclick1=true
+					const typeclick2=false
+					const typeclick3=false
+					const typeclick4=false
+					const rechargetype=1
+					this.props.dispatch({
+							type:'cashier/typeclicks',
+							payload:{typeclick1,typeclick2,typeclick3,typeclick4,rechargetype}
+					})
+			}
+			if(index==2){
+					const typeclick1=false
+					const typeclick2=true
+					const typeclick3=false
+					const typeclick4=false
+					const rechargetype=2
+					this.props.dispatch({
+							type:'cashier/typeclicks',
+							payload:{typeclick1,typeclick2,typeclick3,typeclick4,rechargetype}
+					})
+			}
+			if(index==3){
+					const typeclick1=false
+					const typeclick2=false
+					const typeclick3=true
+					const typeclick4=false
+					const rechargetype=3
+					this.props.dispatch({
+							type:'cashier/typeclicks',
+							payload:{typeclick1,typeclick2,typeclick3,typeclick4,rechargetype}
+					})
+			}
+			if(index==4){
+					const typeclick1=false
+					const typeclick2=false
+					const typeclick3=false
+					const typeclick4=true
+					const rechargetype=4
+					this.props.dispatch({
+							type:'cashier/typeclicks',
+							payload:{typeclick1,typeclick2,typeclick3,typeclick4,rechargetype}
+					})
+			}
+	}
+	reamount=(e)=>{
+		const reamount=e.target.value
+		const re=/^([0-9]*)+((\.)|.[0-9]{1,2})?$/
+		const str=re.test(reamount)
+		if(str){
+				this.props.dispatch({
+						type:'cashier/reamount',
+						payload:reamount
+				})
+		}
+	}
+	reamountblue=(e)=>{
+		const values=e.target.value
+		if(values){
+				const reamount=parseFloat(e.target.value)
+				if(reamount){
+						this.props.dispatch({
+								type:'cashier/reamount',
+								payload:reamount
+						})
+				}
+
+		}
+	}
+	payhindClick=()=>{
+		if(!this.props.reamount || Number(this.props.reamount)<=0){
+				message.warning('金额有误')
+				return
+		}
+		let values={mbCardId:this.props.mbCardId,amount:this.props.reamount,type:this.props.rechargetype}
+		if(values.type=='1'){
+				values.type='7'
+		}
+		if(values.type=='2'){
+				values.type='8'
+		}
+		GetServerData('qerp.pos.mb.card.charge',values)
+		.then((json) => {
+				if(json.code=='0'){
+						const rechargevisible=false
+						this.props.dispatch({
+								type:'cashier/rechargevisible',
+								payload:rechargevisible
+						})
+						const reamount=null
+						this.props.dispatch({
+								type:'cashier/reamount',
+								payload:reamount
+						})
+						setTimeout(()=>{
+								const orderNo=json.chargeNo  //订单号
+								const odOrderId=json.mbCardMoneyChargeId  //订单id
+								const consumeType='2' //充值订单
+								const type=values.type//支付类型
+								const amount=values.amount //支付金额
+								this.context.router.push({ pathname : '/pay', state : {orderId :odOrderId,type:type,amount:amount,consumeType:consumeType,orderNo:orderNo}});
+						})
+				}else{
+						message.warning(json.message)
+						this.context.router.push({ pathname : '/pay', state : {orderId :'100',type:'7',amount:'100',consumeType:'1',orderNo:'0898u'}});
+				}
+		})
+	}
+	choosePrint=(e)=>{
+			const recheckPrint=e.target.checked
+			this.props.dispatch({
+					type:'cashier/rechangeCheckPrint',
+					payload:recheckPrint
+			})
 	}
 	render(){
+		const { dataSource } =this.state;
+    const openWechat=sessionStorage.getItem("openWechat")
+    const openAlipay=sessionStorage.getItem("openAlipay")
 		return(
-			<div>
+			<div className="uesleft-components-wrap">
 				<div className='clearfix mt30'>
     			<Input
 						autoComplete="off"
@@ -167,10 +438,11 @@ class Operationls extends React.Component {
     				<div className='clearfix cashierbox_t posion'>
               <div className='fl'>
 								<span className='c74'>{this.props.name}</span>
-								<span className='c38 ml10'>{this.props.levelStr}</span>
+								<span className='level-str'>{this.props.levelStr}</span>
+								<span className='member-source'>{this.props.levelStr}</span>
 							</div>
               <div className='fr'>
-								<span className='themecolor level-margin-style' onClick={this.toggleEvent.bind(this)}>切换其他会员</span>
+								<span className='themecolor level-margin-style' onClick={this.toggleEvent.bind(this)}>切换其他会员>></span>
 								<span>{this.props.isBirthMonth=='true'?<span className='birthline'>
 								<span className='line'></span>生日</span>:null}</span>
 							</div>
@@ -179,14 +451,7 @@ class Operationls extends React.Component {
   						<div className='fl tc mt10 memberinfobox1 memberinfoboxlist' >
                 <div className='c74 clearfix'>
 									<p className='fl'>余额</p>
-									<div className='rechargebtn'>
-										<Modales
-											name={this.props.name}
-											cardNo={this.props.cardNo}
-											amount={this.props.memberamount}
-											mbCardId={this.props.mbCardId}
-											searchmemberinfo={this.searchmemberinfo.bind(this)}/>
-									</div>
+									<span onClick={this.showModal} className='themecolor'>充值</span>
 								</div>
                 <p className='c38 p2'>{this.props.memberamount}</p>
               </div>
@@ -203,6 +468,75 @@ class Operationls extends React.Component {
   					</div>
   				</div>
   			</div>
+				<Modal
+					title="切换会员"
+	        visible={this.state.visible}
+	        onCancel={()=>this.onCancel()}
+	        width={420}
+	        closable={true}
+	        className="toggle-modal-wrap"
+	        footer={null}>
+					<div>
+						切换会员
+						<Table
+							bordered
+							pagination={false}
+							rowSelection={this.rowSelection}
+							dataSource={dataSource}
+							columns={columns}/>
+					</div>
+				</Modal>
+				<Modal
+          className='rechargepays'
+          visible={this.props.rechargevisible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={null}
+          width={842}>
+            <div className='clearfix'>
+              <div className='fl'>
+                <div className='rechargepays-list clearfix'>
+                  <div className='fl listl'>会员姓名</div>
+                  <div className='fr listr'>{this.props.name}</div>
+                </div>
+                <div className='rechargepays-list'>
+                  <div className='fl listl'>会员卡号</div>
+                  <div className='fr listr'>{this.props.cardNo}</div>
+                </div>
+                <div className='rechargepays-list'>
+                  <div className='fl listl'>账户余额</div>
+                  <div className='fr listr'>{this.props.amount}</div>
+                </div>
+              </div>
+              <div className='fr'>
+                <ul className='rechargelist'>
+                  <li onClick={this.typelist.bind(this,1)} className={this.props.typeclick1?'rechargetype':'rechargetypeoff'}><Button>微信</Button></li>
+                  <li onClick={this.typelist.bind(this,2)} className={this.props.typeclick2?'rechargetype':'rechargetypeoff'}><Button>支付宝</Button></li>
+                  <li onClick={this.typelist.bind(this,3)} className={this.props.typeclick3?'rechargetype':'rechargetypeoff'}><Button>银联</Button></li>
+                  <li onClick={this.typelist.bind(this,4)} className={this.props.typeclick4?'rechargetype':'rechargetypeoff'}><Button>现金</Button></li>
+                </ul>
+                <div className='rechargeover'>
+                  <Input
+                    autoComplete="off"
+                    value={this.props.reamount}
+                    onChange={this.reamount.bind(this)}
+                    onBlur={this.reamountblue.bind(this)}
+                    ref={(node)=>{this.input=node}}
+                    addonBefore={<Btnbrfore title={this.props.rechargetype=='1'?'微信':(this.props.rechargetype=='2'?'支付宝':(this.props.rechargetype=='3'?'银联':(this.props.rechargetype=='4'?'现金':null)))}/>}
+                    addonAfter={(this.props.rechargetype=='1' && openWechat=='1') ||(this.props.rechargetype=='2' && openAlipay=='1') ?<Btnpay hindClicks={this.payhindClick.bind(this)}/>:null}
+                    autoFocus/>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className='tc rechargeok' onClick={this.handleOk.bind(this)}>
+                  确定
+              </div>
+              <div style={{textAlign:"center",marginTop:"10px"}}>
+                <Checkbox onChange={this.choosePrint.bind(this)} checked={this.props.recheckPrint}>打印小票</Checkbox>
+              </div>
+            </div>
+        </Modal>
 			</div>
 		)
 	}
@@ -224,6 +558,16 @@ function mapStateToProps(state) {
 					 ismember,
 					 thispoint,
 					 barcode,
+					 datasouce,
+					 meths,
+					 recheckPrint,
+					 rechargevisible,
+					 reamount,
+					 rechargetype,
+					 typeclick1,
+					 typeclick2,
+					 typeclick3,
+					 typeclick4,
 					 cardNoMobile} = state.cashier;
 	return {
 						name,
@@ -236,7 +580,17 @@ function mapStateToProps(state) {
 						ismember,
 						thispoint,
 						barcode,
-						cardNoMobile
+						cardNoMobile,
+						datasouce,
+						meths,
+						recheckPrint,
+						rechargevisible,
+						reamount,
+						rechargetype,
+						typeclick1,
+						typeclick2,
+						typeclick3,
+						typeclick4,
 					};
 }
 
