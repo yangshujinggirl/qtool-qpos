5b7cab516999ed76f7510659d6b3b437e88efc5d
杨静 <yj@qtoolsbaby.com>
2019-04-17 10:17:19 +0800

    增加文件

---
+++ qspos/src/constants/cashier/components/Btnpay.js
@@ -0,0 +1,22 @@
+import { Table, Input, Icon, Button, Popconfirm ,Tabs,Tooltip ,DatePicker,Select,message,Switch,Modal} from 'antd';
+import Modales from './Modales'
+import ReactDOM from 'react-dom';
+import { connect } from 'dva';
+import {GetServerData} from '../../../services/services';
+
+class Btnpay extends React.Component {
+	state={}
+	hindClick=()=>{
+		this.props.hindClicks()
+	}
+	render(){
+		return(
+			<div style={{cursor:'pointer'}} onClick={this.hindClick.bind(this)}>
+				 <Button className='payscanbtn'>扫码</Button>
+  		</div>
+		)
+	}
+}
+
+
+export default connect()(Btnpay);
