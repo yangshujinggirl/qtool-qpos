ee666e1a2a2cc3949484a63c782d479f0ea1782f
杨静 <yj@qtoolsbaby.com>
2019-10-18 14:25:02 +0800

    商品管理

--- qspos/src/components/Method/Print.jsx
+++ qspos/src/components/Method/Print.jsx
@@ -1666,7 +1666,8 @@ function printSaleOrder(message,printCount){
   LODOP.ADD_PRINT_LINE(posiTopNum,0,posiTopNum,"70mm",3,0);
   posiTopNum = posiTopNum+30;
 
-	LODOP.ADD_PRINT_BARCODE(posiTopNum,"15mm",160,160,"QRCode",`${saleInfoAll.fileDomain}${saleInfoAll.codeUrl}`);
+  LODOP.ADD_PRINT_IMAGE(posiTopNum,"15mm",160,160,"<img src={`${saleInfoAll.fileDomain}${saleInfoAll.codeUrl}`}/>");
+	// LODOP.ADD_PRINT_BARCODE(posiTopNum,"15mm",160,160,"QRCode",`${saleInfoAll.fileDomain}${saleInfoAll.codeUrl}`);
   posiTopNum = posiTopNum+170;
 
 	LODOP.ADD_PRINT_TEXT(posiTopNum,0,"70mm",20,footerContent.scanText);
